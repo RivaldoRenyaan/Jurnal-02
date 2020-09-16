2d190c4f6dae04d0c3b77a55cc744267da17d456
RivaldoRenyaan <71364430+RivaldoRenyaan@users.noreply.github.com>
2020-09-16 11:36:55 +0700

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,9 @@
+public class Program {
+
+	public static void main(String[] args){
+		System.out.println("Nama: Rivaldo Eduard Renyaan");
+		System.out.println("NIM: 6706202042");
+		System.out.println("Kelas: D3IF-44-02");
+	}
+
+}
